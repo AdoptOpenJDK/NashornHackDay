df0c541343448ec38aeeb8064b63a79c90cdfd1f
neomatrix369 <sadhak001@gmail.com>
2013-03-24 15:44:50 +0000

    Tidying files, renaming files, removing .sh files

--- examples/JSON_to_JS_and_Java/JSONObjectClient.java
+++ examples/JSON_to_JS_and_Java/JSONObjectClient.java
@@ -1,6 +1,6 @@
 import org.json.JSONObject;
 
-public class JSONObject_example {
+public class JSONObjectClient {
 	public static void main(String args[]) {
 		JSONObject jsonobject = new JSONObject();
 		System.out.println(jsonobject);
--- examples/JSON_to_JS_and_Java/runNashornViaJar.sh
+++
@@ -1,4 +0,0 @@
-# Script is expecting the "nashorn.jar" to be present in the folder called 'nashorn' and "JSON.jar" in 'JSON-java' in the current path
-# Another way to run nashorn using the -jar parameter: java -jar nashorn/dist/nashorn.jar $1
-#
-java -cp JSON-java:nashorn/dist/nashorn.jar jdk.nashorn.tools.Shell $1
