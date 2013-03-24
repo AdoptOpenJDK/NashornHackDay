bae8d294bfb34d92eaf6bde197e00d89019f43bd
neomatrix369 <sadhak001@gmail.com>
2013-03-24 15:36:15 +0000

    JSON: via JS library and Java library.

---
+++ examples/JSON_to_JS_and_Java/JSONObject_client.java
@@ -0,0 +1,8 @@
+import org.json.JSONObject;
+
+public class JSONObject_example {
+	public static void main(String args[]) {
+		JSONObject jsonobject = new JSONObject();
+		System.out.println(jsonobject);
+	}
+}
---
+++ examples/JSON_to_JS_and_Java/runNashornViaJjs.sh
@@ -0,0 +1,4 @@
+# Script is expecting the jdk1.8 to be present in the /usr/lib/jdk1.8.0/bin/ folder,
+# containing the jrunscript executable
+# Alternate method: bash ../nashorn/bin/jjs <.jjs file name>
+/usr/lib/jdk1.8.0/bin/jrunscript $1
